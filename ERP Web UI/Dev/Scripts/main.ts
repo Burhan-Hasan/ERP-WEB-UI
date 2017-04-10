84ff9f9ba993453f4bd1d20a8b0071161d1a03e9
Burhan-Hasan <burhan.hasanzade@gmail.com>
2017-04-10 10:13:30 +0400

    Navigation scripts was created

---
+++ ERP Web UI/Dev/Scripts/main.ts
@@ -0,0 +1,8 @@
+﻿var $: any;
+$(function () {
+    $('#main-nav').on('click', (e: any) => {
+        var currentLi = e.target.closest('li');
+        $(currentLi).siblings().removeClass('--active');
+        $(currentLi).addClass('--active');
+    });
+});
